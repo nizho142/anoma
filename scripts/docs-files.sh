03eb37d85d2ae01bc5d3ade33140d4f9343004eb
nizho <141626791+nizho142@users.noreply.github.com>
2025-07-19 01:16:47 +0530

    Update docs-files.sh

--- scripts/docs-files.sh
+++ scripts/docs-files.sh
@@ -7,4 +7,4 @@ mkdir -p $destination_folder
 # Find all folders named "files" and copy their contents to the destination folder
 find ./documentation -type d -name "files" -exec sh -c 'cp -r "$0"/* "$1"' {} "$destination_folder" \;
 
-echo "All files copied and merged into $destination_folder"
+echo "All files copied and merged into $destination_folder" #new
